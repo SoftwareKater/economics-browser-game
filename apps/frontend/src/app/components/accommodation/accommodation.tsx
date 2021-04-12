41cb2ab0964e51b18aa1c96adf7b54f4d601737e
SoftwareKater <softwarekater@gmail.com>
2021-04-12 18:09:59 +0200

    fix accommodations master-detail-view

--- apps/frontend/src/app/components/accommodation/accommodation.tsx
+++ apps/frontend/src/app/components/accommodation/accommodation.tsx
@@ -1,44 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './accommodation.scss';
 import { useAccommodationsQuery } from '@economics1k/data-access';
 
 import { InfoCard, DetailCard, DetailCardProps } from '@economics1k/ui';
 
+interface AccommodationComponentState {
+  selectedAccommodationId: string;
+  accommodationDetails?: DetailCardProps;
+}
+
 export const Accommodation = () => {
+  const [value, setValue] = useState<AccommodationComponentState>({
+    selectedAccommodationId: '',
+    accommodationDetails: {
+      id: '',
+      title: 'Your accommodation here!',
+      image: 'assets/placeholder.png',
+      alt: 'dummy',
+      description: 'acc.description',
+      buttonTitle: 'Build',
+      buttonAction: () => {
+        window.alert('building a shack');
+      },
+    },
+  });
+
   const { loading, error, data } = useAccommodationsQuery();
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  let accommodationDetails: DetailCardProps = {} as DetailCardProps;
-
   function updateAccommodationDetails(id: string): void {
-    console.log('updating details ', id);
     const acc = data?.accommodations.find(
       (accommodation) => accommodation.id === id
     );
     if (!acc) {
       return;
     }
-    accommodationDetails = {
-      id: acc.id,
-      title: acc.name,
-      image: 'assets/shack.png',
-      alt: acc.name,
-      description: 'acc.description',
-      buttonTitle: 'Build',
-      buttonAction: () => {
-        window.alert('building a shack');
+    setValue({
+      ...value,
+      accommodationDetails: {
+        id: acc.id,
+        title: acc.name,
+        image: 'assets/shack.png',
+        alt: acc.name,
+        description: acc.description,
+        buttonTitle: 'Build',
+        buttonAction: () => {
+          window.alert('building a shack');
+        },
       },
-    };
-    console.log(accommodationDetails);
+    });
   }
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error :(</p>;
+  }
   return (
     <section>
       <div>
-        {accommodationDetails.id ? (
-          <DetailCard key={accommodationDetails.id} {...accommodationDetails} />
+        {value.accommodationDetails?.id ? (
+          <DetailCard
+            key={value.accommodationDetails.id}
+            {...value.accommodationDetails}
+          />
         ) : (
           <div></div>
         )}
